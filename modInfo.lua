111884e2b13cccbbefd0fc3c7dc09b7a2bb49e3b
ManuWar <manupithulk@gmail.com>
2023-02-13 00:31:54 +0100

    Versión 0.3.8.2 completamente traducida al Españo
    Versión 0.3.8.2 completamente traducida al Español

---
+++ modInfo.lua
@@ -0,0 +1,14 @@
+
+local modInfo = {
+    name = "Sapiens en Español",
+    description = "Este mod es una traducion al Español del juego! \n  1 - Activa El Mod\n  2 - Ajustes\n  3 - En Lenguaje, Selecione Español\n  4 - Pulsa Relanzar.",
+    preview = "preview.png",
+    version = "0.3.8.2",
+    type = "app",
+    developer = "ManuWar9",
+    website = "https://github.com/manuwar9/Sapiens-ES_ES-Traduccion/releases",
+    loadOrder = 1, --Orden de carga predeterminado para todos los activos de este mod. Los números más altos se cargan después de los números más bajos. 
+                   --Para los scripts lua, esto se puede anular opcionalmente en la tabla de mods por script lua.
+}
+
+return modInfo
