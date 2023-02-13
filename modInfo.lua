b3e1a8aeb00010d2079e4d9600f8ba2d5c7664d6
ManuWar4K <manupithulk@gmail.com>
2023-02-13 10:23:34 +0100

    Version 0.3.8.2 fully translated

--- modInfo.lua
+++ modInfo.lua
@@ -6,7 +6,7 @@ local modInfo = {
     version = "0.3.8.2",
     type = "app",
     developer = "ManuWar4K",
-    website = "https://github.com/ManuWar4K/Sapiens-ES_ES-Traduccion/releases",
+    website = "https://github.com/ManuWar4K/Sapiens-ES_ES-Translation",
     loadOrder = 1, --Orden de carga predeterminado para todos los activos de este mod. Los números más altos se cargan después de los números más bajos. 
                    --Para los scripts lua, esto se puede anular opcionalmente en la tabla de mods por script lua.
 }
